43d9ac834e5b357defb1b5879960719111b76044
MariamEwas <maryemhasan2004@gmail.com>
2024-11-22 20:49:42 +0200

    add folders

---
+++ Backend/src/Database_Layer/configdb.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+//import dotenv from 'dotenv';
+
+// Load environment variables
+//dotenv.config();
+
+// Get the database URL from environment variables
+const dbURI: string = 'mongodb+srv://MRB:MARBN12@cluster0.83945.mongodb.net/financeTracker';
+
+// Database connection function
+const connectDB = async (): Promise<void> => {
+  try {
+    await mongoose.connect(dbURI);  
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('Database connection failed:', error);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
