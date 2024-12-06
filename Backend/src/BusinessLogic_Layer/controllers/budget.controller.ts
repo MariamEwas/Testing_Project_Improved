6b6b0e7d0ac47f330d8e2f5b9abe3fe3352fa043
MariamEwas <maryemhasan2004@gmail.com>
2024-12-07 00:03:57 +0200

    fix budget

--- Backend/src/BusinessLogic_Layer/controllers/budget.controller.ts
+++ Backend/src/BusinessLogic_Layer/controllers/budget.controller.ts
@@ -5,16 +5,23 @@ import { JwtPayload } from 'jsonwebtoken';
 
 class BudgetController {
 
+  //inject the service into the controller
+  constructor(private budgetService:BudgetService){}
 
 
   async getAllBudgets(req: Request  & { user?: JwtPayload }, res: Response) {
     try {
+      
+      //not found the user
       const userId = req.user?.id;
       if (!userId) throw new Error("User ID not found in request.");
 
-      const budgets = await BudgetService.getAllBudgets(userId);
+      //call the method from the service to get all budgets
+      const budgets = await this.budgetService.getAllBudgets(userId);
       res.status(200).json(budgets);
-    } catch (error: any) {
+    } 
+    
+    catch (error: any) {
       res.status(400).json({ error: error.message });
     }
   }
@@ -24,12 +31,15 @@ class BudgetController {
 
   async addBudget(req: Request  & { user?: JwtPayload }, res: Response) {
     try {
+
+      //not found user
       const userId = req.user?.id;
       if (!userId) throw new Error("User ID not found in request.");
 
+      //destructure of the body
       const { category, limit } = req.body;
 
-      const newBudget = await BudgetService.addBudget({ category, limit, userId });
+      const newBudget = await this.budgetService.addBudget({ category, limit, userId });
       res.status(201).json(newBudget);
     } catch (error: any) {
       res.status(400).json({ error: error.message });
@@ -41,15 +51,18 @@ class BudgetController {
 
   async editBudget(req: Request & { user?: JwtPayload }, res: Response) {
     try {
+
+      //not found user
       const userId = req.user?.id;
       if (!userId) throw new Error("User ID not found in request.");
 
 
       const updateData = req.body;
 
-
-      const updatedBudget = await BudgetService.editBudget(updateData, userId);
+      //get the data from the service
+      const updatedBudget = await this.budgetService.editBudget(updateData, userId);
       res.status(200).json(updatedBudget);
+
     } catch (error: any) {
       res.status(400).json({ error: error.message });
     }
@@ -57,16 +70,17 @@ class BudgetController {
 
 
 
-
+  //method to delete the budget
   async deleteBudget(req: Request  & { user?: JwtPayload }, res: Response) {
     try {
       const userId = req.user?.id;
       if (!userId) throw new Error("User ID not found in request.");
 
+      //get the budget from params
       const budgetId = req.params.id;
-      console.log(budgetId);
 
-      const deletedBudget = await BudgetService.deleteBudget(budgetId, userId);
+      //delete the budget and return it  
+      const deletedBudget = await this.budgetService.deleteBudget(budgetId, userId);
       res.status(200).json(deletedBudget);
     } catch (error: any) {
       res.status(400).json({ error: error.message });
@@ -74,4 +88,4 @@ class BudgetController {
   }
 }
 
-export default new BudgetController();
+export default BudgetController;
--- Backend/src/BusinessLogic_Layer/routes/budget.routes.ts
+++ Backend/src/BusinessLogic_Layer/routes/budget.routes.ts
@@ -1,19 +1,22 @@
 import express from "express";
 import BudgetController from "../controllers/budget.controller";
 import authenticateToken from '../Middleware/AuthMiddleware';
+import BudgetService from "../services/budget.service";
 
 const router = express.Router();
 
+const budgetService = new BudgetService();
+const budgetController = new BudgetController(budgetService);
 // Get all budgets for a user
-router.get("/my-budgets", authenticateToken, BudgetController.getAllBudgets);
+router.get("/my-budgets", authenticateToken, (req,res)=>budgetController.getAllBudgets(req,res));
 
 // Add a new budget
-router.post("/add-budget", authenticateToken, BudgetController.addBudget);
+router.post("/add-budget", authenticateToken, (req,res)=>budgetController.addBudget(req,res));
 
 // Edit an existing budget (including category updates)
-router.put("/my-budgets", authenticateToken, BudgetController.editBudget);
+router.put("/my-budgets", authenticateToken, (req,res)=>budgetController.editBudget(req,res));
 
 // Delete a budget
-router.delete("/my-budgets/:id", authenticateToken, BudgetController.deleteBudget);
+router.delete("/my-budgets/:id", authenticateToken, (req,res)=>budgetController.deleteBudget(req,res));
 
 export default router;
